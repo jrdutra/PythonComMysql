aede2c6aea42506851f04dc990efb4b6a1610523
Joao Ricardo Dutra <jrdutra.com.br@gmail.com>
2020-09-24 13:28:09 -0300

    Conexão do python com o mysql

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="10.40.10.232",
     user="jrdutra",
-    password="Jrcdutra1",
+    password="123456",
     database="laravel_joao"
 )
 
